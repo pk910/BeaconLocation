ec89e9a108eff6527c1855b5c7c348068e33f291
DominikGebhart <DominikGebhart@users.noreply.github.com>
2016-07-29 01:00:35 +0200

    * Added junit testing framework dependencies * Filled example Unittest with stuff that produces test results

--- app/src/test/java/de/dhbwloerrach/beaconlocation/models/BeaconTest.java
+++ app/src/test/java/de/dhbwloerrach/beaconlocation/models/BeaconTest.java
@@ -1,6 +1,8 @@
 package de.dhbwloerrach.beaconlocation.models;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by enio on 7/29/16.
@@ -9,11 +11,17 @@ public class BeaconTest {
 
     @org.junit.Test
     public void testGetUuid() throws Exception {
-
+        Beacon testbeacon = new Beacon();
+        String uuid = "asdf";
+        testbeacon.setUuid(uuid);
+        assertThat(testbeacon.getUuid(), is(uuid));
     }
 
     @org.junit.Test
     public void testSetUuid() throws Exception {
-
+        Beacon testbeacon = new Beacon();
+        String uuid = "asdf";
+        testbeacon.setUuid(uuid);
+        assert(testbeacon.getUuid().equals(uuid));
     }
 }
