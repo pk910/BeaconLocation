36782b97169f36447c73ea2c11aa9b081348f52c
DominikGebhart <DominikGebhart@users.noreply.github.com>
2016-07-29 00:40:26 +0200

    Added 2 Dummy unit-tests so the new layout of tests in the project/android view is visible. UNIT tests go into "test", Integration tests go into "androidTest" - these get executed by a different gradle task and need a connection to a device or emulator. Those Unittests in "test" get executed in the local JVM. This allows to execute unittests frequently without the need of a connected device. Integration tests can then be run separately when needed.

---
+++ app/src/test/java/de/dhbwloerrach/beaconlocation/database/DatabaseHandlerTest.java
@@ -0,0 +1,16 @@
+package de.dhbwloerrach.beaconlocation.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by enio on 7/29/16.
+ */
+public class DatabaseHandlerTest {
+
+    @Test
+    public void testGetBeacon() throws Exception {
+
+    }
+}
---
+++ app/src/test/java/de/dhbwloerrach/beaconlocation/models/BeaconTest.java
@@ -0,0 +1,19 @@
+package de.dhbwloerrach.beaconlocation.models;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by enio on 7/29/16.
+ */
+public class BeaconTest {
+
+    @org.junit.Test
+    public void testGetUuid() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetUuid() throws Exception {
+
+    }
+}
