6624aeec2804eb7923000aeb52645c6e3747d30f
holzenky <yannick@holzenkamp.org>
2016-09-14 11:36:09 +0200

    Display additional weather information 2

--- app/src/main/java/de/dhbwloerrach/beaconlocation/activities/WeatherFragment.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/activities/WeatherFragment.java
@@ -122,6 +122,25 @@ public class WeatherFragment extends BaseFragment implements WeatherListener {
         strb.append(", Max: ");
         strb.append(weather.getTempMax());
         strb.append(")\n");
+        strb.append("Wind: ");
+        strb.append(weather.getWindAvg());
+        strb.append(" ");
+        strb.append(weather.getWindDir());
+        strb.append(" (Min: ");
+        strb.append(weather.getWindMin());
+        strb.append(", Max: ");
+        strb.append(weather.getWindMax());
+        strb.append(")\n");
+        strb.append("Precipitation Probability: ");
+        strb.append(weather.getRainPossibility());
+        strb.append("%\n");
+        strb.append("Rel. Humidity: ");
+        strb.append(weather.getHumidityAvg());
+        strb.append(" (Min: ");
+        strb.append(weather.getHumidityMin());
+        strb.append(", Max: ");
+        strb.append(weather.getHumidityMax());
+        strb.append(")\n");
 
         ImageView weatherImageView = (ImageView) currentView.findViewById(R.id.weatherImage);
         weatherImageView.setImageResource(weather.getCodeDayPic());
